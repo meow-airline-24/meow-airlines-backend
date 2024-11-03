30239c1215de65b4620ddfaf17488ecd1a9693f6
Asayami <asayamikurashin@gmail.com>
2024-11-03 23:17:03 +0700

    Update auth

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,3 +1,97 @@
-import express, { Router } from "express";
-const router = Router();
-export default router;
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import User from "../models/users.js";
+
+import { DEBUG } from "../../env.js";
+
+/**
+ * Salt is to be embedded into JWT so that with the same base data, tokens are generated different each time.
+ * For this purpose, salts are also required to be different each time they are acquired.
+ */
+function generateSalt() {
+    return Date.now().toString() + (Math.random() * 19).toString();
+}
+
+function generateAccessToken(user, expiresIn = (DEBUG ? "15d" : "1d")) {
+    return jwt.sign(
+        {
+            id: user._id,
+            email: user.email,
+            salt: generateSalt()
+        },
+        process.env.JWT_ACCESS_SECRET,
+        {
+            expiresIn
+        }
+    );
+}
+
+function generateRefreshToken(user, expiresIn = "365d") {
+    return jwt.sign(
+        {
+            id: user._id,
+            email: user.email,
+            salt: generateSalt()
+        },
+        process.env.JWT_REFRESH_SECRET,
+        {
+            expiresIn
+        }
+    );
+}
+
+const refreshTokens = [];
+
+export async function login(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+        throw new HttpException(404, "No user with such email address");
+    }
+
+    const isMatch = await bcrypt.compare(password, user.pwhash);
+    if (!isMatch) {
+        throw new HttpException(401, "Incorrect password");
+    }
+
+    const accessToken = generateAccessToken(user);
+    const refreshToken = generateRefreshToken(user);
+    refreshTokens.push(refreshToken);
+
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: false, // if true, HTTPS only
+        path: "/"
+    });
+
+    res.status(200).json({
+        accessToken
+    });
+}
+
+export async function refreshAccessToken(req, res) {
+    const { refreshToken } = req.cookies;
+
+    if (!refreshToken) {
+        throw new HttpException(400, "No refresh token");
+    }
+    if (!refreshTokens.includes(refreshToken)) {
+        throw new HttpException(403, "Refresh token is invalid");
+    }
+
+    let decoded;
+    try {
+        decoded = jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET);
+    } catch (error) {
+        throw new HttpException(401, "Invalid refresh token");
+    }
+
+    const user = await User.findById(decoded.id);
+    if (!user) throw new HttpException(404, "User not found");
+
+    const accessToken = generateAccessToken(user);
+    res.status(200).json({
+        accessToken
+    });
+}
--- src/routes/api/auth.js
+++ src/routes/api/auth.js
@@ -1,3 +1,11 @@
-import express, { Router } from "express";
-const router = Router();
-export default router;
+import { Router } from "express";
+
+import { login, refreshAccessToken } from "../../controllers/auth.js";
+
+const authRouter = Router();
+
+authRouter.post("/login", (login));
+
+authRouter.post("/refresh_access_token", (refreshAccessToken));
+
+export default authRouter;
