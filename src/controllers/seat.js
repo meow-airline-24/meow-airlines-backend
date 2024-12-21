1472dd564014bc6e7b57e3dbceee3ca9767cf0e8
fior4neee <twtvfiorainyt@gmail.com>
2024-12-21 15:34:15 +0700

    createFlight create Seat

--- src/controllers/seat.js
+++ src/controllers/seat.js
@@ -2,9 +2,9 @@ import Seat from "../models/seats.js";
 import HttpException from "../exceptions/HttpException.js";
 import Flight from "../models/flights.js";
 import { mongoose, Types } from "mongoose";
+import Aircraft from "../models/aircrafts.js";
 
 export async function createSeatsForFlightId(req, res) {
-  console.log(req.body);
   const {
     flight_id,
     seat_number,
@@ -46,5 +46,24 @@ export async function createSeatsForFlightId(req, res) {
 
     console.error("Error creating seat:", error);
     res.status(error.status || 500).json({ message: error.message });
+  }
+}
+
+export async function createSeats(flight_id, aircraft_id, seat_number, seatClass, availability, price) { // tạo ghế cho máy bay
+  try {
+    // Create a new seat
+    const newSeat = await Seat.create({
+      flight_id,
+      aircraft_id: aircraft_id,
+      seat_number,
+      class: seatClass,
+      availability: availability ?? true, // Default to true if not provided
+      price,
+    });
+
+    return newSeat; // Return the created seat object (so we can accumulate them)
+  } catch (error) {
+    console.error("Error creating seat:", error);
+    throw error; // Throw the error to propagate it upwards
   }
 }
